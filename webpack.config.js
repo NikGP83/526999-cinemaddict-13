ea195a7ae1144e3724718f83c148d05dbe434fd3
Nikolay Golovin <nik.golovin83@gmail.com>
2020-11-17 23:35:25 +0600

    сохранение работы

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  entry: './src/main.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  }
+  },
+  devtool: 'source-map'
 }
