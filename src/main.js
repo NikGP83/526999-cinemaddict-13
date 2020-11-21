793176a49b783bcbf1c3882f2bcf167938acc203
Nikolay Golovin <nik.golovin83@gmail.com>
2020-11-21 22:24:27 +0600

    шапка и меню

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import {createUserRankTemplate} from './view/user-rank';
+import {createUserRankTemplate} from './view/user-rank.js';
+import {mainNavigationMenu} from './view/main-navigation-menu.js';
+
 
 const render = (container, template, place) => {
-  container.insertAdjacentHtml(place, template);
-}
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteHeader = document.querySelector(`.header`);
+
+
+render(siteHeader, createUserRankTemplate(), `beforeend`); // в DOM отрисовалась, на странице нет
+
+const siteMainElement = document.querySelector(`.main`);
+render(siteMainElement, mainNavigationMenu(), `afterbegin`);
