516250ef5c53dc36c5f3b29371e974e750b0b140
Nikolay Golovin <nik.golovin83@gmail.com>
2020-11-22 15:27:20 +0600

    добавил карточки фильмов

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import {createUserRankTemplate} from './view/user-rank.js';
 import {mainNavigationMenu} from './view/main-navigation-menu.js';
-
+import {createFIlterList} from './view/filter-list.js';
+import {createItemBoard} from './view/item-board.js';
+import {createFilmCard} from './view/film-card.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -9,7 +11,16 @@ const render = (container, template, place) => {
 const siteHeader = document.querySelector(`.header`);
 
 
-render(siteHeader, createUserRankTemplate(), `beforeend`); // в DOM отрисовалась, на странице нет
+render(siteHeader, createUserRankTemplate(), `beforeend`);
 
 const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, mainNavigationMenu(), `afterbegin`);
+render(siteMainElement, createFIlterList(), `beforeend`);
+render(siteMainElement, createItemBoard(), `beforeend`);
+
+const filmsListContainer = siteMainElement.querySelector(`.films-list__container`);
+const MAX_FILM_CARDS = 5;
+
+for (let i = 0; i <= MAX_FILM_CARDS - 1; i++) {
+  render(filmsListContainer, createFilmCard(), `beforeend`);
+}
