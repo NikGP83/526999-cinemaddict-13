1b066add77de3645d84335cdfebf15dde5446828
Nikolay Golovin <nik.golovin83@gmail.com>
2020-12-01 20:51:35 +0600

    описана структура карточки фильма

--- src/mock/data-template.js
+++ src/mock/data-template.js
@@ -16,25 +16,39 @@ const postersArr = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sageb
   `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`
 ];
 const filmName = [`Фильм один`, `Фильм два`, `Фильм три`];
+const commentDate = [`22.10`, `14.12`, `13.12`]; // возможно фунция с dayjs
+const productDate = [`2001`, `1983`, `2020`];
+const durationTime = [`1h 36m`, `36m`, `3h 45m`];
+const genreName = [`Comedy`, `Drama`, `Musical`];
 
 const getAuthorName = () => authorName[getRandom(0, authorName.length - 1)];
 const getComments = () => commentsArray[getRandom(0, 5)];
 const getFilmDescription = () => descriptionList[getRandom(0, 5)];
 const getPoster = () => postersArr[getRandom(0, postersArr.length - 1)];
 const getFilmName = () => filmName[getRandom(0, filmName.length - 1)];
+const getProductDate = () => productDate[getRandom(0, productDate.length - 1)];
+const getDuration = () => durationTime[getRandom(0, durationTime.length - 1)];
+const getGenreName = () => genreName[getRandom(0, genreName.length - 1)];
 
 export const generatefilmCard = () => {
   return {
-    name: getFilmName(),
-    poster: `/public/images/posters ${getPoster()}`,
-    descriprion: getFilmDescription()
+    filmName: getFilmName(),
+    poster: `/public/images/posters/${getPoster()}`,
+    date: getProductDate(),
+    duration: getDuration(),
+    genre: getGenreName(),
+    descriprion: getFilmDescription(),
+    rating: getRandom(1, 9),
+    commentsNum: getRandom(0, 199)
   };
 };
 
 export const generateComments = () => {
   return {
     name: getAuthorName(),
+    emotion: getEmotion(),
     data: getDate(),
-    comments: getComments()
+    comments: getComments(),
+    deleteBtn: deleteBtn()
   };
 };
