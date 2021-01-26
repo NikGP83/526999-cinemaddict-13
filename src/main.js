c3056c672a0fdd1ca851c00bec7e0f27f757552f
Nikolay Golovin <nik.golovin83@gmail.com>
2021-01-26 20:44:21 +0600

    перенесены все методы из точки входа

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import MainNavigationMenuView from './view/main-navigation-menu.js';
 import FIlterListView from './view/filter-list.js';
 import ItemBoardView from './view/item-board.js';
-import FilmCardView from './view/film-card.js';
-import ShowMoreBtnView from './view/show-more-btn.js';
 import ExtraFilmBoardView from './view/extra-film-board';
 import ExtraFilmBoardCommentedView from './view/extra-filmboard-commented';
 import FooterFilmStatisticsView from './view/footer-film-statistics';
-import PopupBoardView from './view/popup-board.js';
 import {getFilmData} from './mock/data-template.js';
-import {getCommentsData} from './mock/data-template.js';
 import {getfiltersDataNum} from './mock/filter.js';
-import {RenderPosition, render, remove} from './render.js';
+import {RenderPosition, render} from './render.js';
 import UserRankPresenter from './presenter/user-rank.js';
 import MovieListPresenter from './presenter/movie-list.js';
 
@@ -21,6 +17,7 @@ const userRankPresenter = new UserRankPresenter(document.querySelector(`.header`
 userRankPresenter.init();
 
 const siteMainElement = document.querySelector(`.main`);
+const footerContainer = document.querySelector(`.footer`);
 
 render(siteMainElement, new MainNavigationMenuView(filters), RenderPosition.AFTERBEGIN);
 render(siteMainElement, new FIlterListView(), RenderPosition.BEFOREEND);
@@ -29,7 +26,7 @@ render(siteMainElement, new ItemBoardView(), RenderPosition.BEFOREEND);
 const filmMainSection = siteMainElement.querySelector(`.films`);
 const filmsListContainer = filmMainSection.querySelector(`.films-list__container`);
 
-new MovieListPresenter(siteMainElement, filmsListContainer).init(filmProfiles);
+new MovieListPresenter(siteMainElement, filmsListContainer, footerContainer).init(filmProfiles);
 // const closePopup = (el) => {
 //   document.querySelector(`body`).classList.remove(`hide-overflow`);
 //   el.parentElement.removeChild(el);
@@ -38,19 +35,9 @@ new MovieListPresenter(siteMainElement, filmsListContainer).init(filmProfiles);
 // const openPopup = (film) => {
 //   document.querySelector(`body`).classList.add(`hide-overflow`);
 //   const currentFilmComments = getCommentsData().filter((el) => el.filmId === film.id);
-//   const popup = new PopupBoardView(film, currentFilmComments, closePopup);
+  // const popup = new PopupBoardView(film, currentFilmComments, closePopup);
 //   render(footerContainer, popup, RenderPosition.AFTEREND);
 // };
-
-// const limit = Math.min(MAX_FULL_FILM_CARDS, filmProfiles.length);
-// for (let i = 0; i < limit; i++) {
-//   const currentFilmProfile = filmProfiles[i];
-//   const callback = () => {
-//     openPopup(currentFilmProfile);
-//   };
-//   const view = new FilmCardView(currentFilmProfile, callback);
-//   render(filmsListContainer, view, RenderPosition.BEFOREEND);
-// }
 
 // render(filmMainSection, new ExtraFilmBoardView(), RenderPosition.BEFOREEND);
 // render(filmMainSection, new ExtraFilmBoardCommentedView(), RenderPosition.BEFOREEND);
@@ -64,7 +51,7 @@ const extraFilmListCommented = siteMainElement.querySelector(`.films-list--extra
 //   render(extraFilmListCommented, new FilmCardView(filmProfiles[i]), RenderPosition.BEFOREEND);
 // }
 
-const footerContainer = document.querySelector(`.footer`);
+
 const footerStatisticsContainer = document.querySelector(`.footer .footer__statistics`);
 
 render(footerStatisticsContainer, new FooterFilmStatisticsView(), RenderPosition.BEFOREEND);
--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -1,14 +1,17 @@
 import FilmCardView from '../view/film-card.js';
 import {render, RenderPosition, remove} from '../render.js';
 import ShowMoreBtnView from '../view/show-more-btn.js';
+import {getCommentsData} from '../mock/data-template.js';
+import PopupBoardView from '../view/popup-board.js';
 
 const MAX_FULL_FILM_CARDS = 5;
 const FILM_CARDS_PER_STEP = 5;
 
 export default class MovieList {
-  constructor(siteMainElement, filmsListContainer) {
+  constructor(siteMainElement, filmsListContainer, footerContainer) {
     this._siteMainElement = siteMainElement;
     this._filmsListContainer = filmsListContainer;
+    this._footerContainer = footerContainer;
 
     this._filmCardView = null;
     this._ShowMoreBtnView = new ShowMoreBtnView();
@@ -21,14 +24,26 @@ export default class MovieList {
     this._renderShowMoreBtnView(filmProfiles);
   }
 
+  _closePopup(el) {
+    document.querySelector(`body`).classList.remove(`hide-overflow`);
+    el.parentElement.removeChild(el);
+  }
+
+  _openPopup(film) {
+    document.querySelector(`body`).classList.add(`hide-overflow`);
+    const currentFilmComments = getCommentsData().filter((el) => el.filmId === film.id);
+    const popup = new PopupBoardView(film, currentFilmComments, this._closePopup);
+    render(this._footerContainer, popup, RenderPosition.AFTEREND);
+  }
+
   _renderFilmCard(filmCard) {
     const limit = Math.min(MAX_FULL_FILM_CARDS, filmCard.length);
     for (let i = 0; i < limit; i++) {
       const currentFilmProfile = filmCard[i];
-      // const callback = () => {
-      //   openPopup(currentFilmProfile);
-      // };
-      const view = new FilmCardView(currentFilmProfile);
+      const callback = () => {
+        _openPopup(currentFilmProfile);
+      };
+      const view = new FilmCardView(currentFilmProfile, callback);
       render(this._filmsListContainer, view, RenderPosition.BEFOREEND);
     }
   }
